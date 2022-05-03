99eb95eaa7dfa8768b60cd9f618ffe77ef7c8394
Flowyh <altermydestiny@gmail.com>
2022-05-03 20:39:25 +0200

    Pretty Julia documentation (3rd approach)

--- sofixit/liquid_helium_2022/3rd-approach/run_tests.jl
+++ sofixit/liquid_helium_2022/3rd-approach/run_tests.jl
@@ -1,6 +1,9 @@
 include("./LHCargo.jl")
 include("../tests.jl")
 
+"""
+Run test units and assert computed capacity with expected values.
+"""
 function run_tests()
   for test in Tests
     println("Test: $test")
@@ -10,10 +13,14 @@ function run_tests()
   end
 end
 
+"""
+Main program function.
+"""
 function main(args::Array{String})
   run_tests()
 end
 
+# Equivalent of Python's if __name__ == "__main__".
 if abspath(PROGRAM_FILE) == @__FILE__
   main(ARGS)
 end
